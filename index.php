2f72369426f7ed769284a213a46f5f9fe5642b06
ShazleenAziem <shazleenaziem10@gmail.com>
2023-02-20 14:39:51 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="login.php" method="post">
+    <label for="idpengguna">ID Pengguna</label>
+    <input type="text" name="idpengguna" id="idpengguna" required>
+    <label for="katalaluan">Kata Laluan</label>
+    <input type="password" name="katalaluan" id="katalaluan" required>
+    <button type="submit">MASUK</button>
+</form>

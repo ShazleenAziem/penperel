f835e904fd424ba9c88d6570ebff3cde38f02e07
ShazleenAziem <shazleenaziem10@gmail.com>
2023-02-26 22:35:35 +0800

    menu

--- admin/index.php
+++ admin/index.php
@@ -24,7 +24,7 @@
 
                     <tr>
                         <td>No.Kad Pengenalan<td>
-                        <td><input type="text" name="nomatrik" required minlenght="12" maxlenght="12"><td>
+                        <td><input type="text" name="nokpwarden" required minlenght="12" maxlenght="12"><td>
                     </tr>
 
                     <tr>
@@ -38,9 +38,68 @@
     </form>
     <?php
     }else{
-        $idwa
+        $idwarden = $_GET['edit'];
+        $sql = "SELECT * FROM warden WHERE idwarden = $idwarden";
+        $row = $conn->query($sql)->fetch_object();
+        ?>
+
+        <form>
+            <input type="hidden" name="idwarden" value="<?php echo $row->idwarden;?>">
+            <fieldset>
+                <lagend>Kemaskini Data Warden</lagend>
+                <table>
+                    <tr>
+                        <td>Nama Warden</td>
+                        <td><input type="text" name="namawarden";?></td>
+                    </tr>
+
+                    <tr>
+                        <td>No.Kad Pengenalan</td>
+                        <td><input type="text" name="nokpwarden" required value=" <?php echo $row->nokpwarden; ?>"minlenght="12" maxlenght="12"></td>
+                    </tr>
+
+                    <tr>
+                        <td colspan ="2">
+                            <button type="submit">SIMPAN</button>
+                            <button type="resit">BATAL</button>
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>
+        </form>
+        <?php
     }
-               
-    
+    ?>
+
+    <table class="table">
+        <tr>
+            <th>Bil</th>
+            <th>Nama Warden</th>
+            <th>No.Kad Pengenalan</th>
+        </tr>
+        <?php
+        $bil = 1;
+        $sql = "SELECT * FROM warden ORDER BY namawarden";
+        $result =$conn->query($sql);
+        while($row = $result->fetch_object()){
+            ?>
+            <tr>
+                <td><?php echo $bil++;?></td>
+                <td><?php echo $row ->namawarden;?></td>
+                <td><?php echo $nokpwarden;?></td>
+                <td>
+                    <a href="index.php>edit =<?php echo $row->idwarden;?>">Edit</a>
+                    <a href="padam.php?idwarden=<?php echo $row->idwarden;?>"onclick="return sahkan()">Padam</a>
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+    <script>
+        function sahkan(){
+            returm confirm('Adakah anda pasti');
+        }
+    </script>
 </body>
 </html>
--- include/conn.php
+++ include/conn.php
@@ -1,8 +1,2 @@
 <?php
-$host='localhost';
-$user ='root';
-$pswd='';
-$dbname='penperel';
-
-$conn = new mysqli($host,$user,$pswd,$dbname);
-session_start();
+$conn = new mysqli('localhost','root','','contoh_crud');

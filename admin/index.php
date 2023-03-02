721e201eec3e5b021457b6983b2206581e339aa9
ShazleenAziem <shazleenaziem10@gmail.com>
2023-03-02 17:17:21 +0800

    logout

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,25 @@
 <?php
 require '../include/conn.php';
+if (!isset($_SESSION['idpengguna'])) {
+    header("Location:../index.php");
+    exit();
+}
+
 $idpengguna = $_SESSION['idpengguna'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+
+
     <h2>nama :
         <?php
         echo $idpengguna;
@@ -33,11 +42,12 @@ $idpengguna = $_SESSION['idpengguna'];
         </tr>
     </table>
     <?php
-    $menu = 'home';# default value
-    if(isset($_GET['menu'])){
-        $menu=$_GET['menu'];
+    $menu = 'home'; # default value
+    if (isset($_GET['menu'])) {
+        $menu = $_GET['menu'];
     }
-    include"$menu.php";
+    include "$menu.php";
     ?>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -3,38 +3,36 @@ require 'include/conn.php';
 $idpengguna = $_POST['idpengguna'];
 $kata = $_POST['kata'];
 
-if($idpengguna == 'admin'){
-    $sql ='SELECT * FROM admin';
+if ($idpengguna == 'admin') {
+    $sql = 'SELECT * FROM admin';
     $row = $conn->query($sql)->fetch_object();
 
-    if(password_verify($kata,$row->kata)){
-        $_SESSION['idpengguna']='admin';
+    if (password_verify($kata, $row->kata)) {
+        $_SESSION['idpengguna'] = 'admin';
         header('location:admin/');
-    }else{}
-        ?>
-        <script>
-            alert('Maaf, kata laluan salah');
-            window.location='./';
+    } else {
+    }
+?>
+    <script>
+        alert('Maaf, kata laluan salah');
+        window.location = './';
+    </script>
+    <?php
+} else {
+    $sql = "SELECT idwarden, kata FROM warden WHERE nokpwarden ='$idpengguna ";
+    $result = $conn->query($sql);
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_object();
+        if (password_verify($kata, $row->kata)) {
+            $_SESSION['idwarden'] = $row->idwarden;
+            header('location:staff/');
+        } else {
+    ?>
+            <script>
+                alert('Maaf, kata laluan salah.');
+                window.location = './';
             </script>
-            <?php
-    }
-    else{
-        $sql = "SELECT idwarden, kata FROM warden WHERE nokpwarden ='$idpengguna ";
-        $result = $conn->query($sql);
-            if($result->num_rows ==1){
-            $row =$result->fetch_object();
-            if(password_verify($kata,$row->kata)){
-                $_SESSION['idwarden']=$row->idwarden;
-                header('location:staff/');
-            }else{
-                ?>
-                <script>
-                    alert('Maaf, kata laluan salah.');
-                    window.location='./';
-                    </script>
-                    <?php
-            }
+<?php
         }
-        
     }
-
+}
